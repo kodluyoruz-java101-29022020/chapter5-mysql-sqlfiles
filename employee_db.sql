2634966c26d7ab0a8fc2dea8627d931b9a21acb5
Batuhan Düzgün <batuhan.duzgun@std.yeditepe.edu.tr>
2020-04-09 20:41:06 +0300

    Update employee_db.sql

--- employee_db.sql
+++ employee_db.sql
@@ -10,6 +10,16 @@ CREATE TABLE employees (
     PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE employees_auto_inc (
+  emp_no INT(11) NOT NULL AUTO_INCREMENT,
+  birth_date DATE NOT NULL,
+  first_name VARCHAR(14) NOT NULL,
+  last_name VARCHAR(16) NOT NULL,
+  gender ENUM('M','F') NOT NULL,
+  hire_date DATE NOT NULL,
+  PRIMARY KEY (emp_no)
+);
+
 CREATE TABLE departments (
     dept_no     CHAR(4)         NOT NULL,
     dept_name   VARCHAR(40)     NOT NULL,
